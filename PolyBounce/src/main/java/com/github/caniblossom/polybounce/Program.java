165f9c57192af9585ffa84c703075bc91be5d0d0
Jani Salo <jani.q.salo@helsinki.fi>
2015-03-10 23:34:23 +0200

    Revert "Fix messed up commit"
    This reverts commit f23a62bfb2403ef6becadc0712bdf3e78335d8ac.

--- PolyBounce/src/main/java/com/github/caniblossom/polybounce/Program.java
+++ PolyBounce/src/main/java/com/github/caniblossom/polybounce/Program.java
@@ -1,47 +1,16 @@
 /*
- * Copyright (c) 2015, Jani Salo
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package com.github.caniblossom.polybounce;
 
-import com.github.caniblossom.polybounce.game.Game;
-
 /**
- * Main class.
+ * Program entry point.
  * @author Jani Salo
  */
 public class Program {
-    /**
-     * Program entry point.
-     * @param args arguments for execution
-     */
     public static void main(String [] args) {
-        final Game game = new Game();
-        game.run();
-    }   
+        System.out.println("Poly Bounce");
+    }
 }
